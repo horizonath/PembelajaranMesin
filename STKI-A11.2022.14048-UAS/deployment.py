6d10e40fa8ea10efc86368b7a75125b0160dbb2e
Natanael Anggit <83002072+horizonath@users.noreply.github.com>
2025-01-19 21:14:59 +0700

    Update deployment.py

--- STKI-A11.2022.14048-UAS/deployment.py
+++ STKI-A11.2022.14048-UAS/deployment.py
@@ -20,7 +20,8 @@ st.title("📊 Customer Segmentation Dashboard")
 st.markdown("Dashboard ini digunakan untdf menganalisis data pelanggan menggunakan metode RFM (Recency, Frequency, Monetary).")
 
 data_file = 'https://raw.githubusercontent.com/horizonath/PembelajaranMesin/main/STKI-A11.2022.14048-UAS/OnlineRetail.csv'
-df = pd.read_csv(data_file)
+df = pd.read_csv(data_file ,on_bad_lines='skip', encoding='unicode_escape')
+st.dataframe(df.head())
 
 
 # Deskripsi data
